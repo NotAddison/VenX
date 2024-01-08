969353647a21f0b18d05853acd2d61cb5a738571
Addison <S10222525@connect.np.edu.sg>
2024-01-08 22:18:50 +0800

    chore: init file structure
    - define project file structure

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'VenX',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
