4387d4ddc943504093710d21d09cfdad9a94b9db
Addison <S10222525@connect.np.edu.sg>
2024-01-09 23:46:44 +0800

    chore: file structure
    - clean & improve file structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
---
+++ lib/utils/themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+ThemeData defaultTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
