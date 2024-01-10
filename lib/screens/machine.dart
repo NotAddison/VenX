e47dcf03f048b0f7eba78e83dd2f2f0563554b00
Addison <S10222525@connect.np.edu.sg>
2024-01-11 02:59:05 +0800

    chore: init placeholders
    - add placeholders

---
+++ lib/screens/machine.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MachineView extends StatefulWidget {
+  const MachineView({super.key});
+
+  @override
+  State<MachineView> createState() => _MachineViewState();
+}
+
+class _MachineViewState extends State<MachineView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
