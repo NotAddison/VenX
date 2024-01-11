6f13f7bb52e65e132e297ffd73afca1a73c301cf
Addison <S10222525@connect.np.edu.sg>
2024-01-12 02:27:24 +0800

    feat: disable mobile rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // Screens
 import './widgets/main_base.dart';
@@ -16,6 +17,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         title: 'VenX',
         debugShowCheckedModeBanner: false,
