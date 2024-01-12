18759c88fa2e7791b16059090a0aae56cc1a0fb5
Addison <S10222525@connect.np.edu.sg>
2024-01-12 16:29:51 +0800

    feat: machine page - page to display full machine info

--- lib/screens/machine.dart
+++ lib/screens/machine.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import '../models/machine_schema.dart';
+import '../widgets/sub_base.dart';
+import '../widgets/preview_machine.dart';
+import '../widgets/card_stock.dart';
 
 class MachineView extends StatefulWidget {
-  const MachineView({super.key});
+  final Machine machine;
+  const MachineView({super.key, required this.machine});
 
   @override
   State<MachineView> createState() => _MachineViewState();
@@ -10,6 +15,67 @@ class MachineView extends StatefulWidget {
 class _MachineViewState extends State<MachineView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Base(
+      title: widget.machine.title,
+      showReturnButton: false,
+      body: Container(
+          child: Padding(
+        padding:
+            const EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 30),
+        child: Column(
+          children: [
+            // Preview
+            MachinePreview(machine: widget.machine, isCard: true),
+
+            // Spacer
+            const SizedBox(height: 40),
+
+            // Item stock List
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.machine.stocks.length,
+                itemBuilder: (context, index) {
+                  return StockCard(stock: widget.machine.stocks[index]);
+                },
+              ),
+            ),
+
+            // Spacer
+            const SizedBox(height: 20),
+
+            // Column, button at the end of the screen
+            Column(
+              children: [
+                // Button
+                MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  child: const Text("Dietrition / AI Nutritionist"),
+                ),
+
+                // Spacer
+                const SizedBox(height: 10),
+
+                MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  child: const Text("Return"),
+                )
+              ],
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
